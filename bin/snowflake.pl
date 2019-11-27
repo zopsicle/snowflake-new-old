38c8c195c553fb23fd86925178d994df5574c197
Chloé <rightfold+ck@gmail.com>
2019-11-27 20:52:30 +0100

    Catch errors thrown by rules file

--- bin/snowflake.pl
+++ bin/snowflake.pl
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
 
+use Carp qw(croak);
 use File::Spec;
 use Snowflake::Config;
 
@@ -11,6 +12,11 @@ if (@ARGV != 1) {
 my $rules_file = File::Spec->rel2abs($ARGV[0]);
 
 my @rules = do $rules_file;
+if (@rules == 0) {
+    if ($@ ne '') { croak($@); }
+    if ($! ne '') { croak($!); }
+    croak('Unknown error');
+}
 
 my $config = Snowflake::Config->new(stash_path => 'build');
 $_->get_output_hash($config) for @rules;
