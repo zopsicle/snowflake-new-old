6d6bcf380f863386062ff18ae0112bc3885b404e
Chloé <rightfold+ck@gmail.com>
2020-01-28 19:37:39 +0100

    Pass remaining arguments to rules file

--- bin/snowflake.pl
+++ bin/snowflake.pl
@@ -8,11 +8,12 @@ use Carp qw(confess);
 use File::Spec;
 use Snowflake::Config;
 
-if (@ARGV != 1) {
-    say STDERR 'Usage: snowflake RULESFILE';
-    exit(1);
-}
-my $rules_file = File::Spec->rel2abs($ARGV[0]);
+my $rules_file = File::Spec->rel2abs(
+    shift(@ARGV) // do {
+        say STDERR 'Usage: snowflake RULESFILE ...';
+        exit(1);
+    },
+);
 
 my %artifacts = do $rules_file;
 if (keys(%artifacts) == 0) {
